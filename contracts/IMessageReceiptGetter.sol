fdf0b760e45f1496d8988682919c8413536a8c12
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2022-01-07 13:58:29 -0800

    Changed interface for reciepts to no longer also implement message receiver

--- contracts/IMessageReceiptGetter.sol
+++ contracts/IMessageReceiptGetter.sol
@@ -1,12 +1,10 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-import "./IMessageReceiver.sol";
-
 /**
  * @dev Interface for receiving message sent receipts from bridge
  */
-interface IMessageReceiverWithReceipt is IMessageReceiver {
+interface IMessageReceiptGetter {
 
 	// MUST only be callable by bridge network
 	function messageReceipt(
