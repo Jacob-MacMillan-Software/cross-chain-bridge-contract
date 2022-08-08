19809e53de9bbb03b76d06b01aecefe1fa8bfd28
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2022-08-08 16:27:14 -0700

    Added more error tests

--- test/toll_bridge_errors.ts
+++ test/toll_bridge_errors.ts
@@ -1,7 +1,10 @@
 import { expect } from "chai";
 import { ethers, upgrades } from "hardhat";
-// eslint-disable-next-line node/no-missing-import
-import { generateFeeData } from "./helpers/messageSigning";
+import {
+  generateFeeData,
+  generateHashedMessage,
+  // eslint-disable-next-line node/no-missing-import
+} from "./helpers/messageSigning";
 
 const ERC20Mock = require("../artifacts/contracts/mocks/ERC20Mock.sol/ERC20Mock.json");
 // const ERC721Mock = require("../artifacts/contracts/mocks/ERC721Mock.sol/ERC721Mock.json");
@@ -290,6 +293,155 @@ describe("Toll Bridge Errors", function () {
           // @ts-ignore
           err.message.includes(
             "reverted with custom error 'FunctionNotPayable()'"
+          )
+        ) {
+          failed = true;
+        }
+      }
+
+      // eslint-disable-next-line no-unused-expressions
+      expect(failed).to.be.true;
+    });
+
+    it("fail to Transfer a fungilbe token to another network with a modified fee verification (signature is valid, but from a different verification. Fee amount changed to 0, all other elements unchanged)", async function () {
+      const [owner, addr1] = await ethers.getSigners();
+      const abi = new ethers.utils.AbiCoder();
+      const types = ["address", "uint256", "uint256", "bytes32", "bytes"];
+
+      const Bridge = await ethers.getContractFactory("TollBridge");
+      const bridge = await upgrades.deployProxy(Bridge, [
+        owner.address,
+        addr1.address,
+        1,
+      ]);
+      await bridge.deployed();
+
+      await mockERC20.approve(bridge.address, 0xffffffffff);
+
+      // Generate fee verification
+      const feeData = await generateFeeData(
+        owner.address,
+        2,
+        ethers.constants.AddressZero,
+        feeAmount,
+        noExpireBlock,
+        { tokenAddr: mockERC20.address, tokenAmount: 100 },
+        null,
+        false,
+        addr1,
+        bridge
+      );
+
+      // Decode the feeData
+      const [feeToken, oldFeeAmount, maxBlock, hash, signature] = abi.decode(
+        types,
+        feeData
+      );
+
+      // Verify this was correctly decoded
+      expect(feeToken).to.equal(ethers.constants.AddressZero);
+      expect(oldFeeAmount).to.equal(feeAmount);
+      expect(maxBlock).to.equal(noExpireBlock);
+
+      const newFeeAmount = 0;
+      const forgedFeeData = abi.encode(types, [
+        feeToken,
+        newFeeAmount,
+        maxBlock,
+        hash,
+        signature,
+      ]);
+
+      let failed = false;
+      try {
+        await bridge.transferFungible(mockERC20.address, 100, 2, forgedFeeData);
+      } catch (err) {
+        if (
+          // @ts-ignore
+          err.message.includes(
+            'reverted with custom error \'HashMismatch("0xe36f6c1541ec1a332d2e95c86eeae27c2b70b7e85481512c7c6b8999ad3fe734", "0xd5d3023786a79e8bc9e22cad26fc4b086faff56200394f86593e8800324bb1f5")\''
+          )
+        ) {
+          failed = true;
+        }
+      }
+
+      // eslint-disable-next-line no-unused-expressions
+      expect(failed).to.be.true;
+    });
+
+    it("fail to Transfer a fungilbe token to another network with a modified fee verification (signature is valid, but from a different verification. Fee amount changed to 0 and hashed changed to match, all other elements unchanged)", async function () {
+      const [owner, addr1, addr2] = await ethers.getSigners();
+      const abi = new ethers.utils.AbiCoder();
+      const types = ["address", "uint256", "uint256", "bytes32", "bytes"];
+
+      const Bridge = await ethers.getContractFactory("TollBridge");
+      const bridge = await upgrades.deployProxy(Bridge, [
+        owner.address,
+        addr1.address,
+        1,
+      ]);
+      await bridge.deployed();
+
+      await mockERC20.approve(bridge.address, 0xffffffffff);
+
+      // Generate fee verification
+      const feeData = await generateFeeData(
+        owner.address,
+        2,
+        ethers.constants.AddressZero,
+        feeAmount,
+        noExpireBlock,
+        { tokenAddr: mockERC20.address, tokenAmount: 100 },
+        null,
+        false,
+        addr1,
+        bridge
+      );
+
+      // Decode the feeData
+      const [feeToken, oldFeeAmount, maxBlock, , signature] = abi.decode(
+        types,
+        feeData
+      );
+
+      // Verify this was correctly decoded
+      expect(feeToken).to.equal(ethers.constants.AddressZero);
+      expect(oldFeeAmount).to.equal(feeAmount);
+      expect(maxBlock).to.equal(noExpireBlock);
+
+      const newFeeAmount = 0;
+
+      // Hash new data
+      const [hash] = await generateHashedMessage(
+        await bridge.chainId(),
+        owner.address,
+        2,
+        ethers.constants.AddressZero,
+        newFeeAmount,
+        noExpireBlock,
+        { tokenAddr: mockERC20.address, tokenAmount: 100, tokenId: null },
+        null,
+        false,
+        addr2
+      );
+
+      const forgedFeeData = abi.encode(types, [
+        feeToken,
+        newFeeAmount,
+        maxBlock,
+        hash,
+        signature,
+      ]);
+
+      let failed = false;
+      try {
+        await bridge.transferFungible(mockERC20.address, 100, 2, forgedFeeData);
+      } catch (err) {
+        if (
+          // @ts-ignore
+          err.message.includes(
+            "reverted with custom error 'UntrustedSigner(\"0x5c26722a3A215E2dB53bbE442b95E531d565aFFe\")'"
           )
         ) {
           failed = true;
